009d2c434388ebc52f37ed1c0e7044af0ab55eac
Blake <cleancode.blake@gmail.com>
2022-03-06 23:37:06 -0500

    Change Nav Color using pathname

---
+++ components/NavBar.js
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function NavBar() {
+  const router = useRouter();
+  console.log(router.pathname);
+  return (
+    <div>
+      <nav>
+        <Link href="/">
+          <a className={`${router.pathname === "/" ? "active" : ""}`}>HOME</a>
+        </Link>
+        <Link href="/about">
+          <a className={`${router.pathname === "/about" ? "active" : ""}`}>
+            ABOUT
+          </a>
+        </Link>
+      </nav>
+
+      <style jsx>{`
+        a {
+          text-decoration: none;
+          color: black;
+          margin-right: 10px;
+        }
+        .active {
+          color: tomato;
+          font-weight: bold;
+        }
+      `}</style>
+    </div>
+  );
+}

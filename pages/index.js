f3086d4d657e366e425e0d9bbb9d4fb40ebcfc14
Blake <cleancode.blake@gmail.com>
2022-03-07 23:39:43 -0500

    Create _app file to use design globally

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
 import { useRouter } from "next/router";
 import { useState } from "react";
-import NavBar from "../components/NavBar";
 
 export default function Home() {
   const router = useRouter();
   // console.log(router);
   return (
     <div>
-      <NavBar></NavBar>
       <h1>THIS IS HOME</h1>
     </div>
   );

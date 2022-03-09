07fcd7595793ea0b578029b49b7fdcc50a37c718
Blake <cleancode.blake@gmail.com>
2022-03-09 00:48:33 -0500

    Create layout component to implement layout pattern

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
-import NavBar from "../components/navbar";
+import Layout from "../components/layout";
+
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <NavBar />
+    <Layout>
       <Component {...pageProps} />
-    </>
+    </Layout>
   );
 }

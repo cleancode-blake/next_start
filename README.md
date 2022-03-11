8a097fbe81974c25aa93711d39d48a39264f1bd0
Blake <cleancode.blake@gmail.com>
2022-03-10 23:57:41 -0500

    Update Login Credentials

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 #### 1. How to import/use local/web image in nextJS
 
 #### 2. Fetch the data in the Home and Paint
+
+#### After watching NEXTJS tutorial => move to Tailwind lecture
+
+#### AFter practicing tailwind => move to nextjs projects
+
+#### After nextJS projects => move to nextJS with TS
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "b628b0f65cc8570a11e0e393168cfb49";
+const MOVIE_API =
+  "https://api.themoviedb.org/3/movie/latest?api_key=<<api_key>>&language=en-US";
+
 export default function Home() {
   const router = useRouter();
 
